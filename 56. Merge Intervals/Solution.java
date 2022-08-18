28be1eaaccd4386b61de1bdaa169d2f21f8d4d2f
aman.lalwani <aman.lalwani@innovaccer.com>
2022-08-19 01:38:08 +0530

    more questions

--- 56. Merge Intervals/Solution.java
+++ 56. Merge Intervals/Solution.java
@@ -41,3 +41,36 @@ class Solution {
         
     }
 }
+
+
+//without using stack  and extra space
+
+
+
+
+// class Solution {
+//     public int[][] merge(int[][] intervals) {
+//         if(intervals.length<=1||intervals==null){
+//             return intervals;
+//         }
+//         List<int[]> res= new ArrayList<>();
+//         Arrays.sort(intervals,(arr1,arr2)-> Integer.compare(arr1[0], arr2[0]));
+//         int start= intervals[0][0];
+//         int end=intervals[0][1];
+//         for(int[] i:intervals){
+//             if(i[0]<=end){
+//                 end=Math.max(end,i[1]);
+//             }
+//             else {
+//                 res.add(new int[]{start,end});
+//                 start=i[0];
+//                 end=i[1];
+//             }
+                        
+//         }
+//         res.add(new int[]{start,end});
+//         return res.toArray(new int[0][]);
+       
+        
+//     }
+// }
