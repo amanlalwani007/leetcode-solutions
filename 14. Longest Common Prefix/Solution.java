05965910d6b55129826424ad2b25f6e99f224d15
aman.lalwani <aman.lalwani@innovaccer.com>
2022-08-25 07:09:42 +0530

    add more question

--- 14. Longest Common Prefix/Solution.java
+++ 14. Longest Common Prefix/Solution.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Solution {
     public String longestCommonPrefix(String[] strs) {
 
--- 31. Next Permutation/Solution.java
+++ 31. Next Permutation/Solution.java
@@ -1,58 +1,44 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n=nums.length;
-        int k =n-2;
-        for(int i= n-1;i>0;i--){
-            if(nums[i]<=nums[i-1]){
+        int n = nums.length;
+        int k = n - 2;
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i] <= nums[i - 1]) {
                 k--;
-            }
-            else {
+            } else {
                 break;
             }
 
         }
-        if(k==-1){
-            reverse(nums, 0, n-1);
+        if (k == -1) {
+            reverse(nums, 0, n - 1);
             return;
         }
-        for(int i= n-1;i>0;i--){
-          if(nums[i]>nums[k]){
-              int temp = nums[k];
-              nums[k]=nums[i];
-              nums[i]=temp;
-              break;
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i] > nums[k]) {
+                int temp = nums[k];
+                nums[k] = nums[i];
+                nums[i] = temp;
+                break;
 
-          }
-          
+            }
 
         }
-        reverse(nums, k+1, n-1);
+        reverse(nums, k + 1, n - 1);
 
-
-        
-
-        
     }
 
-
-    void reverse(int[] nums,int start,int  end){
-        while(start<end){
-            int temp=nums[start];
-            nums[start]=nums[end];
-            nums[end]=temp;
+    void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
             start++;
             end--;
         }
 
-
-    } 
+    }
 }
 
-
-
-//13542-> 14532
-//step 1 find breaking points where   nums[i-1]< nums [i] where i=n-1
-
-
-
-
+// 13542-> 14532
+// step 1 find breaking points where nums[i-1]< nums [i] where i=n-1
