12e09ff179b0fdf8a78b2b44237e2170bc00679a
amanlalwani007 <amanlalwani0807@gmail.com>
2023-01-19 01:39:46 +0000

    more questions

---
+++ 326. Power of Three/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        return n>0 && 1162261467%n==0;
+    }
+}
+
+//  approach is we fnc max 3 to power value in integer datatyoe then divide by number if it give 0 then it is divisible 
+
